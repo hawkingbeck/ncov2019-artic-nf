24984dc5eb6fa8152624b68dc3058cc4494448ce
Sam Proctor <sam.proctor@proctor-analytics.com>
2022-05-13 15:01:38 +0100

    update installation script

--- installationScript.sh
+++ installationScript.sh
@@ -6,3 +6,9 @@ sudo apt-get -y install zlib1g-dev
 sudo apt-get -y install libncurses5-dev
 sudo apt-get -y install libncursesw5-dev
 sudo apt-get -y install liblzma-dev
+
+mkdir ~/downloads
+cd ~/downloads
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh /S /D=%UserProfile%\Miniconda3
+source ~/.profile

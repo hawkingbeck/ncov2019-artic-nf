4a9301a8c227e18358e732298edd03be94ba3c82
Sam Proctor <sam.proctor@proctor-analytics.com>
2022-05-13 15:01:00 +0100

    update installation script

--- installationScript.sh
+++ installationScript.sh
@@ -1,2 +1,8 @@
 sudo apt-get -y update
 sudo apt-get -y install gcc
+sudo apt-get -y install make
+sudo apt-get -y install libbz2-dev
+sudo apt-get -y install zlib1g-dev
+sudo apt-get -y install libncurses5-dev
+sudo apt-get -y install libncursesw5-dev
+sudo apt-get -y install liblzma-dev
